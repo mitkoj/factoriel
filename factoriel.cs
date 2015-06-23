4ccd35a71c47ac79811aa6eb3d7e76e604ebe620
mitkoj <mitak98@gmail.com>
2015-06-23 23:08:21 +0300

    Create factoriel.cs

---
+++ factoriel.cs
@@ -0,0 +1,20 @@
+using System;
+
+class CalculateNK
+{
+    static void Main()
+    {
+        Console.Write("Positive int > 0 and < 100: ");
+            byte n = byte.Parse(Console.ReadLine());
+            Console.Write("Positive int > 0 and < N: ");
+            byte k = byte.Parse(Console.ReadLine());
+            k++;
+
+            double answer = 1;
+            for (byte i = k; i <= n; i++)
+            {
+                answer *= i;
+            }
+            Console.WriteLine("The answer is: " + answer);
+    }
+}
